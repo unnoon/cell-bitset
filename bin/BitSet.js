6c40918b3fab72a73dddd965ec791dee44d466cd
grogger.io <r.geertzema@gmail.com>
2017-09-18 11:49:56 +0200

    0.3.2

--- bin/BitSet.js
+++ bin/BitSet.js
@@ -777,7 +777,7 @@ export default class BitSet {
 BitSet.info = {
     "name": "cell-bitset",
     "description": "Fast JS BitSet implementation. Beyond 32bit restrictions.",
-    "version": "0.3.1",
+    "version": "0.3.2",
     "url": "https://github.com/unnoon/cell-bitset",
 };
 /* tslint:enable:quotemark object-literal-key-quotes */
